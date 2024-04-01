b6ef8f897d5001c093cf522261bdbd4046e2037c
xu_yanfeng <xu_yanfeng@126.com>
2024-04-01 15:57:00 +0800

    多定点渲染

--- index.ts
+++ index.ts
@@ -20,9 +20,12 @@ varying vec2 v_texCoord;
 uniform sampler2D u_sampler;
 void main()
 {
-    gl_FragColor = texture2D(u_sampler, v_texCoord);
+    vec4 color = texture2D(u_sampler, v_texCoord);
+    gl_FragColor = color;
 }
 `;
+
+const items: Array<{ x: number; y: number; w: number; h: number }> = [];
 function main() {
   const canvas: HTMLElement | null = document.getElementById("canvas");
   if (!canvas) return;
@@ -32,8 +35,10 @@ function main() {
     return;
   }
   // 背景色
-  gl.clearColor(0, 1, 0, 1);
+  gl.clearColor(0.5, 0.5, 0.5, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.enable(gl.BLEND);
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
   const program = shader(gl);
   if (!program) {
     return;
@@ -42,9 +47,8 @@ function main() {
   createTexture(gl);
   const local = gl.getUniformLocation(program, "u_sampler");
   gl.uniform1i(local, 0);
-
   // 绘制
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  gl.drawElements(gl.TRIANGLES, 6 * items.length, gl.UNSIGNED_SHORT, 0);
 }
 function shader(gl: WebGLRenderingContext) {
   // 顶点着色器
@@ -84,27 +88,50 @@ function shader(gl: WebGLRenderingContext) {
 }
 
 function fillVertices(gl: WebGLRenderingContext, program: WebGLProgram) {
-  const verticesTexCoords = new Float32Array([
-    //
-    -0.5,
-    0.5,
-    0.0,
-    1.0, //
-    -0.5,
-    -0.5,
-    0.0,
-    0.0, //
-    0.5,
-    0.5,
-    1.0,
-    1.0, //
-    0.5,
-    -0.5,
-    1.0,
-    0.0, //
-  ]);
-  const buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  items.push({ x: -0.5, y: 0.5, w: 1, h: 1 });
+  items.push({ x: -0.6, y: 0.6, w: 1, h: 1 });
+  const verticesTexCoords = new Float32Array(items.length * 4 * 4);
+  const indicesTexCoords = new Uint16Array(items.length * 6);
+  for (let i = 0; i < items.length; i++) {
+    const { x, y, w, h } = items[i];
+    const left = x,
+      right = x + w,
+      top = y,
+      bottom = y - h;
+    /**
+     * 0    2
+     *
+     * 1    3
+     */
+    indicesTexCoords.set(
+      [0, 1, 3, 3, 2, 0].map((item) => item + i * 4),
+      i * 6
+    );
+    verticesTexCoords.set(
+      [
+        left,
+        top,
+        0.0,
+        1.0, //
+        left,
+        bottom,
+        0.0,
+        0.0, //
+        right,
+        top,
+        1.0,
+        1.0, //
+        right,
+        bottom,
+        1.0,
+        0.0, //
+      ],
+      i * 4 * 4
+    );
+  }
+
+  const buffer_vertices = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer_vertices);
   gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
   const size = verticesTexCoords.BYTES_PER_ELEMENT;
   const local_position = gl.getAttribLocation(program, "a_position");
@@ -114,7 +141,10 @@ function fillVertices(gl: WebGLRenderingContext, program: WebGLProgram) {
   const local_texCoord = gl.getAttribLocation(program, "a_texCoord");
   gl.vertexAttribPointer(local_texCoord, 2, gl.FLOAT, false, size * 4, size * 2);
   gl.enableVertexAttribArray(local_texCoord);
-  return buffer;
+
+  const buffer_indices = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer_indices);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indicesTexCoords, gl.STATIC_DRAW);
 }
 function createTexture(gl: WebGLRenderingContext) {
   const texture = gl.createTexture();
